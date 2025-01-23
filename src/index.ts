27c3d5ecaf5633ecf124f74611d1d68f38db446e
legend80s <liuchzong@qq.com>
2025-01-23 23:28:40 +0800

    feat: and choice of colorful background

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ const svgIcons = {
 
   success: `<svg viewBox="64 64 896 896" focusable="false" data-icon="check-circle" width="1em" height="1em" fill="currentColor" aria-hidden="true"><path d="M512 64C264.6 64 64 264.6 64 512s200.6 448 448 448 448-200.6 448-448S759.4 64 512 64zm193.5 301.7l-210.6 292a31.8 31.8 0 01-51.7 0L318.5 484.9c-3.8-5.3 0-12.7 6.5-12.7h46.9c10.2 0 19.9 4.9 25.9 13.3l71.2 98.8 157.2-218c6-8.3 15.6-13.3 25.9-13.3H699c6.5 0 10.3 7.4 6.5 12.7z"></path></svg>`,
 
-  loading: `<svg color="#1677ff" viewBox="0 0 1024 1024" focusable="false" data-icon="loading" fill="currentColor" aria-hidden="true" style=" animation: loadingCircle 1s infinite linear; "><path d="M988 548c-19.9 0-36-16.1-36-36 0-59.4-11.6-117-34.6-171.3a440.45 440.45 0 00-94.3-139.9 437.71 437.71 0 00-139.9-94.3C629 83.6 571.4 72 512 72c-19.9 0-36-16.1-36-36s16.1-36 36-36c69.1 0 136.2 13.5 199.3 40.3C772.3 66 827 103 874 150c47 47 83.9 101.8 109.7 162.7 26.7 63.1 40.2 130.2 40.2 199.3.1 19.9-16 36-35.9 36z"></path></svg>`,
+  loading: `<svg viewBox="0 0 1024 1024" focusable="false" data-icon="loading" fill="currentColor" aria-hidden="true" style=" animation: loadingCircle 1s infinite linear; "><path d="M988 548c-19.9 0-36-16.1-36-36 0-59.4-11.6-117-34.6-171.3a440.45 440.45 0 00-94.3-139.9 437.71 437.71 0 00-139.9-94.3C629 83.6 571.4 72 512 72c-19.9 0-36-16.1-36-36s16.1-36 36-36c69.1 0 136.2 13.5 199.3 40.3C772.3 66 827 103 874 150c47 47 83.9 101.8 109.7 162.7 26.7 63.1 40.2 130.2 40.2 199.3.1 19.9-16 36-35.9 36z"></path></svg>`,
 
   close: `<svg fill-rule="evenodd" viewBox="64 64 896 896" focusable="false" data-icon="close" width="1em" height="1em" fill="currentColor" aria-hidden="true"><path d="M799.86 166.31c.02 0 .04.02.08.06l57.69 57.7c.04.03.05.05.06.08a.12.12 0 010 .06c0 .03-.02.05-.06.09L569.93 512l287.7 287.7c.04.04.05.06.06.09a.12.12 0 010 .07c0 .02-.02.04-.06.08l-57.7 57.69c-.03.04-.05.05-.07.06a.12.12 0 01-.07 0c-.03 0-.05-.02-.09-.06L512 569.93l-287.7 287.7c-.04.04-.06.05-.09.06a.12.12 0 01-.07 0c-.02 0-.04-.02-.08-.06l-57.69-57.7c-.04-.03-.05-.05-.06-.07a.12.12 0 010-.07c0-.03.02-.05.06-.09L454.07 512l-287.7-287.7c-.04-.04-.05-.06-.06-.09a.12.12 0 010-.07c0-.02.02-.04.06-.08l57.7-57.69c.03-.04.05-.05.07-.06a.12.12 0 01.07 0c.03 0 .05.02.09.06L512 454.07l287.7-287.7c.04-.04.06-.05.09-.06a.12.12 0 01.07 0z"></path></svg>`,
 };
@@ -59,53 +59,55 @@ function info(
   open(parseConfig('info', config, duration, onClose));
 }
 
-function success(config: Omit<MessageConfig, 'type'>): void;
-function success(
-  content: string,
-  duration?: number,
-  onClose?: () => void
-): void;
-function success(
+type IMethod = typeof info;
+
+// function success(config: Omit<MessageConfig, 'type'>): void;
+// function success(
+//   content: string,
+//   duration?: number,
+//   onClose?: () => void
+// ): void;
+const success: IMethod = (
   config: Omit<MessageConfig, 'type'> | string,
   duration?: number,
   onClose?: () => void
-) {
+) => {
   open(parseConfig('success', config, duration, onClose));
-}
+};
 
-function warn(config: Omit<MessageConfig, 'type'>): void;
-function warn(content: string, duration?: number, onClose?: () => void): void;
-function warn(
+// function warn(config: Omit<MessageConfig, 'type'>): void;
+// function warn(content: string, duration?: number, onClose?: () => void): void;
+const warn: IMethod = (
   config: Omit<MessageConfig, 'type'> | string,
   duration?: number,
   onClose?: () => void
-) {
+) => {
   open(parseConfig('warn', config, duration, onClose));
-}
+};
 
-function error(config: Omit<MessageConfig, 'type'>): void;
-function error(content: string, duration?: number, onClose?: () => void): void;
-function error(
+// function error(config: Omit<MessageConfig, 'type'>): void;
+// function error(content: string, duration?: number, onClose?: () => void): void;
+const error: IMethod = (
   config: Omit<MessageConfig, 'type'> | string,
   duration?: number,
   onClose?: () => void
-) {
+) => {
   open(parseConfig('error', config, duration, onClose));
-}
+};
 
-function loading(config: Omit<MessageConfig, 'type'>): void;
-function loading(
-  content: string,
-  duration?: number,
-  onClose?: () => void
-): void;
-function loading(
+// function loading(config: Omit<MessageConfig, 'type'>): void;
+// function loading(
+//   content: string,
+//   duration?: number,
+//   onClose?: () => void
+// ): void;
+const loading: IMethod = (
   config: Omit<MessageConfig, 'type'> | string,
   duration?: number,
   onClose?: () => void
-) {
+) => {
   open(parseConfig('loading', config, duration, onClose));
-}
+};
 
 function parseConfig(
   type: ILevel,
